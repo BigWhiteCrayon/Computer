7fbfd4519da189ae8589a5e22bae4e60d91c7c2b
BigWhiteCrayon <jordanryoungberg@gmail.com>
2020-05-20 15:38:26 -0700

    added automatic command handling

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const fs = require('fs');
 const Discord = require('discord.js');
 const { prefix } = require('./config.json');
@@ -12,4 +13,20 @@ for (const file of commandFiles) {
 	client.commands.set(command.name, command);
 }
 
-client.login()
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) return;
+
+	try {
+		client.commands.get(command).execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
+
+client.login(process.env.DISCORD_TOKEN);

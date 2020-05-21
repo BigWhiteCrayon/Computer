09c08791de1efb5b57031a8e11ec0b57d0ce4cfb
BigWhiteCrayon <jordanryoungberg@gmail.com>
2020-05-21 16:43:53 -0700

    made it delete play commands to reduce channel clutter

--- commands/play.js
+++ commands/play.js
@@ -7,20 +7,21 @@ module.exports = {
     async execute(message, args) {
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-            if (args[0]) {
-                let string = '';
-                for (const arg of args) {
-                    string += arg + '%20';
-                }
-                string = string.substring(0, string.length - 3);
-                let url = 'https://www.googleapis.com/youtube/v3/search?key=' + process.env.GOOGLE_API_KEY +
-                    '&part=snippet&maxResults=1&q=' + string + '&type=video';
-                req = unirest('GET', url)
-                    .end(function (res) {
-                        if (res.error) throw new Error(res.error);
-                        const musicURL = 'https://www.youtube.com/watch?v=' + res.body.items[0].id.videoId;
-                        connection.play(ytdl(musicURL, { quality: 'highestaudio' }), {volume: 0.25});
-                    });
+            if (args[0]) {let string = '';
+            for (const arg of args) {
+                string += arg + '%20';
+            }
+            string = string.substring(0, string.length - 3);
+            let url = 'https://www.googleapis.com/youtube/v3/search?key=' + process.env.GOOGLE_API_KEY +
+                '&part=snippet&maxResults=1&q=' + string + '&type=video';
+            req = await unirest('GET', url)
+                .end((res) => {
+                    if (res.error){
+                        console.log(res.error);
+                    }
+                    const musicURL = 'https://www.youtube.com/watch?v=' + res.body.items[0].id.videoId;
+                    connection.play(ytdl(musicURL, { quality: 'highestaudio' }), {volume: 0.25});
+            });
             }
             else {
                 message.channel.send('Please provide a song title')
@@ -29,5 +30,7 @@ module.exports = {
         else {
             message.channel.send('You need to join a channel first');
         }
+        message.delete().catch(console.error);
     },
 };
+

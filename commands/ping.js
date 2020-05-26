319a936421cc751546d5209c636ba0e74ec9ed3a
BigWhiteCrayon <jordanryoungberg@gmail.com>
2020-05-25 19:44:32 -0700

    added a message for jared

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,9 @@ module.exports = {
 	name: 'ping',
 	description: 'Ping!',
 	execute(message, args) {
+		if(message.member.displayName == 'Inheritor'){
+			return message.channel.send('I\'m sorry Jared. I\'m afraid I can\'t do that.', {tts: true});
+		}
 		message.channel.send('I\'m sorry Dave. I\'m afraid I can\'t do that.', {tts: true});
 	},
 };
